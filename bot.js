69220b652c3f378b93570c7b5dfbdad43bf19b33
Denis Labaste <denis@labaste.fr>
2018-12-14 12:40:08 +0100

    fix author id

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ bot.on('ready', function(evt) {
 
 bot.on('message', message => {
 
-    if (message.content.toLowerCase().search(/alco+l/g) > -1 && (message.author.id === bot.user.id)) {
+    if (message.content.toLowerCase().search(/alco+l/g) > -1 && (message.author.id !== bot.user.id)) {
         message.channel.send(`ALCOOOL !`).then(mess => {
             mess.react('🍺');
             mess.react('🍻');
